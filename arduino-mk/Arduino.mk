1012f765fe3ff181550766d615f8c5b41b45c01e
Sudar <sudar@sudarmuthu.com>
2013-06-14 17:49:02 +0530

    Tag v0.10.6 for release

--- arduino-mk/Arduino.mk
+++ arduino-mk/Arduino.mk
@@ -19,7 +19,7 @@
 #
 # Original Arduino adaptation by mellis, eighthave, oli.keller
 #
-# Current version: 0.10.5
+# Current version: 0.10.6
 #
 # Refer to HISTORY.md file for complete history of changes
 #
