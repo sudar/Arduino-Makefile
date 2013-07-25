82fad52f6c63f38b54d341234db4f71c43594b2c
Sudar <sudar@sudarmuthu.com>
2013-07-25 21:30:43 +0530

    Remove duplicate definition of `show_config_variable` macro
    In 5ed507cf43e048f0a6a527c97ea6ed2e8cdb451f as part of #98,
    [`show_config_variable` was duplicated in Arduino.mk file](https://github.com/sudar/Arduino-Makefile/pull/98#commitcomment-3561542)
    
    But it was later found that `ARDMK_DIR` is still not getting printed
    (issue #111). This commit therefore removes the duplicated macro
    definition.

--- arduino-mk/Arduino.mk
+++ arduino-mk/Arduino.mk
@@ -211,12 +211,6 @@ endif
 # Makefile distribution path
 #
 
-# Call with the name of the variable, a prefix tag if desired (like
-# [AUTODETECTED]), and an explanation if desired (like (found in $$PATH). This
-# function definition is duplicated from Common.mk so that it is available
-# before we import that Makefile.
-show_config_variable = $(call show_config_info,$(1) = $($(1)) $(3),$(2))
-
 ifndef ARDMK_DIR
     # presume it's a level above the path to our own file
     ARDMK_DIR := $(realpath $(dir $(realpath $(lastword $(MAKEFILE_LIST))))/..)
