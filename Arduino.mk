b1ab8b45e1f00537279bc62b4a5a3b3e423b8c7c
Chris Patuzzo <chris@patuzzo.co.uk>
2014-05-03 01:36:42 +0100

    If no port is specified, try to guess it from wildcards

--- Arduino.mk
+++ Arduino.mk
@@ -866,8 +866,14 @@ ifeq ($(CURRENT_OS), WINDOWS)
     COM_PORT_ID = $(subst com,,$(MONITOR_PORT))
     COM_STYLE_MONITOR_PORT = com$(COM_PORT_ID)
     DEVICE_PATH = /dev/ttyS$(shell awk 'BEGIN{ print $(COM_PORT_ID) - 1 }')
-else
+endif
+
+ifdef ARDUINO_PORT
     DEVICE_PATH = $(MONITOR_PORT)
+else
+    # If no port is specified, try to guess it from wildcards.
+    DEVICE_PATH = $(firstword $(wildcard \
+			/dev/ttyACM? /dev/ttyUSB? /dev/tty.usbserial* /dev/tty.usbmodem*))
 endif
 
 # Returns the Arduino port (first wildcard expansion) if it exists, otherwise it errors.
