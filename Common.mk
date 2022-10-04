b03ad3402f556296bfddbbe3fb5ae0db6a886dce
Jan Hrach <jenda.jzqk73@hrach.eu>
2022-10-05 01:53:05 +0200

    Fix: 'grep: warning: stray \ before #' in GNU grep >= 3.8

--- Common.mk
+++ Common.mk
@@ -8,7 +8,7 @@ dir_if_exists = $(if $(wildcard $(1)$(2)),$(1))
 # result = $(call READ_BOARD_TXT, 'boardname', 'parameter')
 PARSE_BOARD = $(shell if [ -f $(BOARDS_TXT) ]; \
 then \
-  $(GREP_CMD) -Ev '^\#' $(BOARDS_TXT) | \
+  $(GREP_CMD) -Ev '^#' $(BOARDS_TXT) | \
   $(GREP_CMD) -E "^[ \t]*$(1).$(2)=" | \
   cut -d = -f 2- | \
   cut -d : -f 2; \
